6e738cf2ef4cc869e0554d7d605b40bea95b9b5e
alwen <alwenperara44@gmail.com>
2024-12-05 14:01:04 +0530

    create login and signup page

---
+++ src/main/java/com/Project_Monday/service/CustomerService.java
@@ -0,0 +1,26 @@
+package com.Project_Monday.service;
+
+import com.Project_Monday.model.Customer;
+import com.Project_Monday.repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    public void addCustomer(Customer customer){
+        customerRepo.save(customer);
+    }
+
+    public boolean checkCustomer(String username,String password){
+        Customer customer = customerRepo.findByUsername(username);
+        return customer!=null && customer.getPassword().equals(password);
+    }
+
+    public boolean existingCustomer(String username){
+        return customerRepo.findByUsername(username)!=null;
+    }
+}
