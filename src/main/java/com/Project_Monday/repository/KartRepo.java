f9dafae44c9f54c315c624616d20588fd865e66d
alwen <alwenperara44@gmail.com>
2024-12-09 03:07:07 +0530

    Implement the backend logic for cart page

--- src/main/java/com/Project_Monday/repository/KartRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Project_Monday.repository;
-
-import com.Project_Monday.model.Kart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KartRepo extends JpaRepository<Kart,Long> {
-}
