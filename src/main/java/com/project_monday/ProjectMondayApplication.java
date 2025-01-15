ace544d9e2171e16eee1bd8ee15728d3eee5e508
alwen <alwenperara44@gmail.com>
2025-01-15 13:27:47 +0530

    Initial commit

---
+++ src/main/java/com/project_monday/ProjectMondayApplication.java
@@ -0,0 +1,13 @@
+package com.project_monday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMondayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMondayApplication.class, args);
+	}
+
+}
